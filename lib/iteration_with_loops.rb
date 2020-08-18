325d16f1253daadcb27a95bd77da0114c377a29a
Mary Dick <maryclairedick@gmail.com>
2020-08-18 21:43:23 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,10 +8,12 @@ def find_even_values(src)
     while element_index < src.length do
       if src[row_index][element_index].even?
         p src[row_index][element_index]
-        element_index += 1
+        
       end
-      row_index += 1
+      element_index += 1
+      
     end
+    row_index += 1
   end
 
 end
